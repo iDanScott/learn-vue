da75f1575430b03a3fa9be9be3f0742871135292
Dan Scott 🤓 <dan.scott@anana.com>
2020-07-15 11:27:44 +0100

    finished exercise 9

--- exercise-9/src/App.vue
+++ exercise-9/src/App.vue
@@ -17,21 +17,42 @@
                     <!-- Exercise 3 -->
                     <!-- Edit the Example from above and create a custom "Full Name" Control -->
                     <!-- which still holds the First Name and Last Name Input Field -->
+
+                    <form>
+                        <FullName v-model="userData.name"></FullName>
+                        <div class="form-group">
+                            <label>Mail</label>
+                            <input type="email" class="form-control" v-model="userData.mail" />
+                        </div>
+                        <div class="form-group">
+                            <label>Password</label>
+                            <input type="password" class="form-control" v-model="userData.password" />
+                        </div>
+                        <div class="form-group">
+                            <label>Store Data?</label>
+                            <select class="form-control" v-model="userData.storeInDatabase">
+                                <option value="y">Yes</option>
+                                <option value="n">No</option>
+                            </select>
+                        </div>
+                        <button type="submit" class="btn btn-primary" @click.prevent="submitted">Submit</button>
+                    </form>
+
                 </div>
             </div>
         </form>
         <hr>
-        <div class="row">
+        <div class="row" v-if="isSubmitted">
             <div class="col-xs-12 col-sm-8 col-sm-offset-2 col-md-6 col-md-offset-3">
                 <div class="panel panel-default">
                     <div class="panel-heading">
                         <h4>Your Data</h4>
                     </div>
                     <div class="panel-body">
-                        <p>Full Name: </p>
-                        <p>Mail: </p>
-                        <p>Password: </p>
-                        <p>Store in Database?: </p>
+                        <p>Full Name: {{ fullName }}</p>
+                        <p>Mail: {{ userData.mail }} </p>
+                        <p>Password: {{ userData.password }} </p>
+                        <p>Store in Database?: {{ storeInDatabaseDisplay }}</p>
                     </div>
                 </div>
             </div>
@@ -40,7 +61,39 @@
 </template>
 
 <script>
+    import FullName from './components/FullName.vue';
+
     export default {
+        data() {
+            return {
+                userData: {
+                    mail: '', 
+                    password: '', 
+                    storeInDatabase: 'y', 
+                    name: {
+                        firstName: '', 
+                        lastName: ''
+                    }
+                }, 
+                isSubmitted: false
+            }
+        }, 
+        components: {
+            FullName: FullName,
+        },
+        computed: {
+            fullName() {
+                return `${this.userData.name.firstName} ${this.userData.name.lastName}`
+            }, 
+            storeInDatabaseDisplay() {
+                return this.userData.storeInDatabase === 'y' ? 'Yes' : 'No'
+            }
+        }, 
+        methods: {
+            submitted() {
+                this.isSubmitted = 'true';
+            }
+        }
     }
 </script>
 
---
+++ exercise-9/src/components/FullName.vue
@@ -0,0 +1,31 @@
+<template>
+    <div>
+        <div class="form-group">
+            <label>First Name</label>
+            <input type="text" class="form-control" v-model="value.firstName" @change="changed()" />
+        </div>
+        <div class="form-group">
+            <label>Last Name</label>
+            <input type="text" class="form-control" v-model="value.lastName" @change="changed()" />
+        </div>
+    </div>
+</template>
+
+<script>
+export default {
+    name: 'FullName',
+    props: ['value'], 
+    methods: {
+        changed() {
+            this.$emit('input', {
+                firstName: this.value.firstName, 
+                lastName: this.value.lastName
+            });
+        }
+    }
+}
+</script>
+
+<style scoped>
+
+</style>
